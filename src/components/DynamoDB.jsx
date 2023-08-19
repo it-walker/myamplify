60b1784606fb9ea9f979ec8236f933577a06bf70
it-walker <24758599+it-walker@users.noreply.github.com>
2023-08-19 23:38:19 +0900

    ストレージの追加

--- src/components/DynamoDB.jsx
+++ src/components/DynamoDB.jsx
@@ -1,13 +1,16 @@
 import { API, GRAPHQL_AUTH_MODE } from '@aws-amplify/api'
+import { Storage } from 'aws-amplify'
 import { useEffect, useState } from "react"
 
 import { createTodo } from '../graphql/mutations'
 import { listTodos } from '../graphql/queries'
+import { S3 } from './S3'
 
 export function DynamoDB() {
   const [todos, setTodos] = useState([])
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
+  const [image, setImage] = useState(null)
 
   useEffect(() => {
     fetchTodos()
@@ -33,15 +36,23 @@ export function DynamoDB() {
     setDescription(event.target.value)
   }
 
+  const handleImageChange = (event) => {
+    setImage(event.target.files[0])
+  }
+
   const handleSubmit = async (event) => {
     event.preventDefault()
 
     try {
+      const stored = await Storage.put(image.name, image, {
+        contentType: image.type,
+      })
+
       const todoDetails = {
         title,
         description,
+        image: stored.key,
       }
-      console.log('todoDetails:', todoDetails)
       const newTodo = await API.graphql({
         query: createTodo,
         variables: { input: todoDetails },
@@ -72,14 +83,21 @@ export function DynamoDB() {
           />
         </label>
         <br/>
+        <label>
+          Image:
+          <input type="file" onChange={handleImageChange}/>
+        </label>
+        <br/>
         <button type="submit">Create Post</button>
       </form>
       <ol>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <p>
-              title={todo.title}, description={todo.description}
-            </p>
+            <S3
+              title={todo.title}
+              description={todo.description}
+              image={todo.image}
+            />
           </li>
         ))}
       </ol>
